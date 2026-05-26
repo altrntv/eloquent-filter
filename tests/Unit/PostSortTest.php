8c3cc174972d69c79655b053e3cc8a91b123ad94
Pavel Dykin <sssecularization@gmail.com>
2026-05-26 19:17:52 +0700

    PostSortTest fix

--- tests/Unit/PostSortTest.php
+++ tests/Unit/PostSortTest.php
@@ -3,22 +3,25 @@
 use Tests\Models\Post;
 
 beforeEach(function () {
-    Post::factory()
-        ->state([
-            'published_at' => '1999-11-10',
-        ])
+    $this->past = now()->subYears(random_int(6, 10));
+    $this->future = now()->addDays(random_int(1, 30));
+
+    Post::factory(25)
         ->create();
 
-    Post::factory(25)->create();
-
-    Post::factory()
-        ->state([
-            'published_at' => '2025-11-10',
-        ])
+    Post::factory(2)
+        ->sequence(
+            [
+                'published_at' => $this->past,
+            ],
+            [
+                'published_at' => $this->future,
+            ],
+        )
         ->create();
 });
 
 it('is first when sorting by published at', function () {
-    expect(Post::sort('published_at')->first())->published_at->toEqual('1999-11-10')
-        ->and(Post::sort('-published_at')->first())->published_at->toEqual('2025-11-10');
+    expect(Post::sort('published_at')->first())->published_at->toEqual($this->past->toDateTimeString())
+        ->and(Post::sort('-published_at')->first())->published_at->toEqual($this->future->toDateTimeString());
 });
